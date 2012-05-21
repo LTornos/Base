27b4d39568fe4be0b7d5333f7da6cecc705cab2f
Laura Tornos <ltornosg@gmail.com>
2012-05-21 17:14:46 +0200

    Load Testing project

--- SourceCode/Base.Presentation.WebApplication/About.aspx.cs
+++ SourceCode/Base.Presentation.WebApplication/About.aspx.cs
@@ -7,6 +7,10 @@ using System.Web.UI.WebControls;
 
 namespace Base.Presentation.WebApplication
 {
+    /// <summary>
+    /// 
+    /// 
+    /// </summary>
     public partial class About : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
